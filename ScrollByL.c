73aecaafd3e4b80c57a2549e399b7f0b769bcf5e
Alan Coopersmith <alan.coopersmith@oracle.com>
2022-12-20 18:58:48 -0800

    configure: Use AC_SYS_LARGEFILE to enable large file support
    While xman can't handle files more than 2gb in size, due to libXt
    API limits, it can handle files with large inodes. (Not that any
    man page should ever be more than even 1mb in size in reality.)
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- ScrollByL.c
+++ ScrollByL.c
@@ -32,6 +32,7 @@ from the X Consortium.
 #include <ctype.h>
 #include <X11/Xos.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #include <X11/IntrinsicP.h>
 #include <sys/stat.h>           /* depends on IntrinsicP.h */
@@ -799,6 +800,13 @@ LoadFile(Widget w)
     if (fstat(fileno(file), &fileinfo))
         XtAppError(XtWidgetToApplicationContext(w),
                    "SBLW LoadFile: Failure in fstat.");
+
+/*
+ * The XtMalloc below is limited to a size of int by the libXt API.
+ */
+    if (fileinfo.st_size >= INT_MAX)
+        XtAppError(XtWidgetToApplicationContext(w),
+                   "SBLW LoadFile: File too large.");
 
 /*
  * Allocate a space for a list of pointer to the beginning of each line.
